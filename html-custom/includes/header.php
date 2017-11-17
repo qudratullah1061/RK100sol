ca744e2e6179e0039a69e858dd4ecc7177fc2cd9
yousiph <muhammadyousiph@gmail.com>
2017-11-17 07:32:03 +0500

    latest changes

---
+++ html-custom/includes/header.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <!-- title -->
+    <title>Konsorts – Creative Agency, Corporate and Portfolio Multi-purpose Template</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="author" content="ThemeZaa">
+    <!-- description -->
+    <meta name="description" content="Konsorts is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos.">
+    <!-- keywords -->
+    <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, coming soon, faq">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    <!-- animation -->
+    <link rel="stylesheet" href="css/animate.css" />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- et line icon -->
+    <link rel="stylesheet" href="css/et-line-icons.css" />
+    <!-- font-awesome icon -->
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <!-- web fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,900|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"  rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800,900" rel="stylesheet">
+    <!-- themify icon -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- swiper carousel -->
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <!-- justified gallery  -->
+    <link rel="stylesheet" href="css/justified-gallery.min.css">
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <!-- revolution slider -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
+    <!-- bootsnav -->
+    <link rel="stylesheet" href="css/bootsnav.css">
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- responsive css -->
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <!--[if IE]>
+            <script src="js/html5shiv.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+    <!-- start header -->
+    <header class="header-with-topbar">
+        <!-- topbar -->
+
+        <!-- end topbar -->
+        <!-- start navigation -->
+        <nav class="navbar navbar-default bootsnav navbar-fixed-top header-light bg-white  on no-full">
+            <div class="container nav-header-container">
+                <div class="row">
+                    <!-- start logo -->
+                    <div class="col-md-2 col-xs-5">
+                        <a href="index.html" title="Konsorts" class="logo">
+                            <img src="img/logo.png" data-at2x="img/logo.png" class="logo-dark" alt="Konsorts">
+                            <img src="img/logo.png" data-at2x="img/logo.png" alt="Konsorts" class="logo-light default">
+                        </a>
+                    </div>
+                    <!-- end logo -->
+                    <div class="col-md-7 col-xs-2 width-auto pull-right accordion-menu xs-no-padding-right">
+                        <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#navbar-collapse-toggle-1">
+                            <span class="sr-only">toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-collapse collapse pull-right" id="navbar-collapse-toggle-1">
+                            <ul id="accordion" class="nav navbar-nav navbar-left no-margin text-normal" data-in="fadeIn" data-out="fadeOut">
+                                <!-- start menu item -->
+                                <li class="active">
+                                    <a href="javascript:void(0);">Home</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0);">About</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0);">Our Services</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0);" title="Blog">Blog</a>
+                                </li>
+                                <li class="">
+                                    <a href="javascript:void(0);">Contact Us</a>
+                                </li>
+                                <li class="dropdown megamenu-fw">
+                                    <a href="javascript:void(0);">Log in</a>
+                                    <i class="fa fa-angle-down dropdown-toggle" data-toggle="dropdown" aria-hidden="true"></i>
+                                    <!-- start sub menu -->
+
+                                </li>
+                                <li>
+                                    <div class="signup-link">
+                                        <a href="#" class="btn btn-small btn-deep-purple lato font-weight-700"> SIGN UP </a>
+                                    </div>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- <div class="col-md-2 col-xs-5 width-auto">
+                        <div class="header-searchbar">
+                            <a href="#search-header" class="header-search-form text-white">
+                                <i class="fa fa-search search-button"></i>
+                            </a>
+                          
+                            <form id="search-header" method="post" action="search-result.html" name="search-header" class="mfp-hide search-form-result">
+                                <div class="search-form position-relative">
+                                    <button type="submit" class="fa fa-search close-search search-button"></button>
+                                    <input type="text" name="search" class="search-input" placeholder="Enter your keywords..." autocomplete="off">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="heder-menu-button sm-display-none">
+                            <button class="navbar-toggle mobile-toggle right-menu-button" type="button" id="showRightPush">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </nav>
+        <!-- end navigation -->
+
+    </header>
+    <!-- end header -->
