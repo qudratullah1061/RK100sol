2e70b57542d5dae745f981d6a86feec38ab1d506
yousiph <muhammadyousiph@gmail.com>
2017-11-20 16:03:39 +0500

    login page, forgot password page reset password page

--- html-custom/includes/header.php
+++ html-custom/includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
-    <meta name="author" content="ThemeZaa">
+    <meta name="author" content="konsortsTeam">
     <!-- description -->
     <meta name="description" content="Konsorts is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos.">
     <!-- keywords -->
---
+++ html-custom/password-rest.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+    <head>
+        <!-- title -->
+        <title>Konsorts – Creative Agency, Corporate and Portfolio Multi-purpose Template</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+        <meta name="author" content="konsortsTeam">
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/login.css" />
+    </head>
+    <body>
+        <div class="">
+            <div class="outer">
+                <div class="middle">
+                    <div class="inner">
+
+                        <div class="panel panel-login panel-reset">
+                            <div class="panel-heading">
+                                <a href="#" class="pointer-none" id="login-form-link"> Reset Password</a>
+                            </div>
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                    
+                                        <form id="forget-password" action="https://phpoll.com/register/process" method="post" role="form" style="">
+                                            <div class="form-group">
+                                                <input type="password" name="passowrd" class="form-control" placeholder="Password" value="">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="passowrd" class="form-control" placeholder="Confirm Password" value="">
+                                            </div>
+
+                                            <div class="form-group text-center">
+                                                <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="btn btn-default" value="Send">
+                                                <!--<div class="col-md-8 text-right"><a href="#" class="forgot-password">Forgot Password?</a></div>-->
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/login.js"></script>
+    </body>
+</html>
