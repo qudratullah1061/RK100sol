4d0fc161bc3857a8cd0e614dc6d58a58be1f88ee
yousiph <muhammadyousiph@gmail.com>
2017-11-22 07:36:36 +0500

    profile pages comleted

---
+++ html-custom/profile.php
@@ -0,0 +1,254 @@
+<?php include_once 'includes/header.php' ?>
+<section class="profle">
+    <div class="container">
+        <div class="col-md-3 col-sm-4">
+            <div class="profile-left">
+                <div class="profile-personal">
+                    <div class="profile-media">
+                        <img src="img/profile-img.jpg" alt="">
+                    </div>
+                    <div class="profile-info">
+                        <p>Robert C.</p>
+                        <span> <i class="fa fa-map-marker"></i> Belgium, Malimont </span>
+                    </div>
+                    <div class="profile-rating">
+                        <span class="profile-points">5.0</span>
+                        <ul class="profile-stars">
+                            <li><i  class="fa fa-star"></i></li>
+                            <li><i  class="fa fa-star"></i></li>
+                            <li><i  class="fa fa-star"></i></li>
+                            <li><i  class="fa fa-star"></i></li>
+                            <li><i  class="fa fa-star"></i></li>
+                        </ul>
+                        <span class="profile-reviews-counter">
+                            2 reviews
+                        </span>
+                    </div>
+                    <div class="text-center">
+                        <a href="#" class="btn btn-deep-purple">Message</a>
+                    </div>
+                </div>
+                <div class="profile-misc">
+                    <ul>
+                        <li><p>Registration Date:</p>  <span>18-Aug,2017</span></li>
+                        <li><p>Date of Birth: </p><span>15-Jan,1973</span></li>
+                        <li><p>Acalability: </p><span class="online"> <i class="fa fa-check-circle"></i>Available</span></li>
+                        <li><p>Travel Option:</p> <span>Off</span></li>
+                        <li><p>Available From:</p></li>
+                        <li><p>Available To:</p></li>
+                        <li><p>Location:</p></li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="profile-language">
+                    <h6>Language:</h6>
+                    <p>French (Native or Bilingual)</p>
+                    <p>German (Conversational)</p>
+                    <p>English (Frank)</p>
+                </div>
+                <div class="profile-social">
+                    <h6>Social Media:</h6>
+                    <ul>
+                        <li class="enabled"><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                        <li class="enabled"><a href="#"><i class="fa fa-youtube-square"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus-square"></i></a></li>
+                        <li class="enabled"><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                        <li class="enabled"><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+                    </ul>
+                </div>
+                <div class="profile-certification">
+                    <div class="profile-cert-head"><h6>Certification:</h6></div>
+                    <div class="certification-odd">
+                        <p>Fitness Trainer (2016)</p>
+                        <span>Lorem Ipsum Foundation</span>                        
+                    </div>
+                    <div class="certification-even">
+                        <p>Online Coaching (2017)</p>
+                        <span>Lorem Ipsum Foundation</span>
+                    </div>
+                </div>
+                <div class="profile-skills">
+                    <ul>
+                        <li><h6>My Skills</h6></li>
+                        <li>Travel Companion</li>
+                        <li>City Tours</li>
+                        <li>Personal Trainer</li>
+                        <li>Running Partner</li>
+                        <li>Hiking Buddy</li>
+                        <li>Fitness Online Coach</li>
+                        <li>Financial Coach</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9 col-sm-8">
+            <div class="profile-right">
+                <div class="profile-intro">
+                    <h5>Tourism, Fitness, Coaching.</h5>
+                    <p>Hi there,</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus. Sit amet nisl purus in mollis nunc sed. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Eget magna fermentum iaculis eu non diam. Vitae proin sagittis nisl rhoncus mattis rhoncus. Feugiat vivamus at augue eget. Egestas diam in arcu cursus. </p>
+                    <p>Enim tortor at auctor urna nunc. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Tellus molestie nunc non blandit massa enim nec dui. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. 
+                        Velit laoreet id donec ultrices tincidunt arcu non. Turpis nunc eget lorem dolor sed viverra. A condimentum vitae sapien pellentesque habitant morbi tristique. In est ante in nibh mauris. Tellus at urna condimentum mattis pellentesque id. Praesent elementum facilisis leo vel fringilla. Id nibh tortor id aliquet.</p>
+                </div>
+
+                <div class="profile-portfolio">
+                    <h5>Portfolio:</h5>
+                    <div class="row">
+                        <div class="col-md-12 no-padding xs-padding-15px-lr">
+                            <div class="filter-content overflow-hidden">
+                                <ul class="portfolio-grid work-4col gutter-large hover-option6 lightbox-portfolio">
+                                    <li class="grid-sizer"></li>
+                                    <!-- start portfolio item -->
+                                    <li class="grid-item wow zoomIn last-paragraph-no-margin">
+                                        <figure>
+                                            <div class="portfolio-img bg-purple position-relative text-center overflow-hidden">
+                                                <img src="img/portfolio-1.jpg" alt=""/>
+                                                <div class="portfolio-icon text-white text-left">
+                                                    <p>Battle fields of </p>
+                                                    <p>Flanders, Belgium</p>
+                                                    <span class="seperator bg-light-gray"></span>
+                                                    <h6>Travel Place</h6>
+                                                </div>
+                                            </div>
+                                        </figure>
+                                    </li>
+                                    <!-- end portfolio item -->
+                                    <!-- start portfolio item -->
+                                    <li class="grid-item wow zoomIn last-paragraph-no-margin" data-wow-delay="0.2s">
+                                        <figure>
+                                            <div class="portfolio-img bg-purple position-relative text-center overflow-hidden">
+                                                <img src="img/portfolio-2.jpg" alt=""/>
+                                                <div class="portfolio-icon text-white text-left">
+                                                    <p>Battle fields of </p>
+                                                    <p>Flanders, Belgium</p>
+                                                    <span class="seperator bg-light-gray"></span>
+                                                    <h6>Travel Place</h6>
+                                                </div>
+                                            </div>
+
+                                        </figure>
+                                    </li>
+                                    <!-- end portfolio item -->
+                                    <!-- start portfolio item -->
+                                    <li class="grid-item wow zoomIn last-paragraph-no-margin" data-wow-delay="0.4s">
+                                        <figure>
+                                            <div class="portfolio-img bg-purple position-relative text-center overflow-hidden">
+                                                <img src="img/portfolio-3.jpg" alt=""/>
+                                                <div class="portfolio-icon text-white text-left">
+                                                    <p>Battle fields of </p>
+                                                    <p>Flanders, Belgium</p>
+                                                    <span class="seperator bg-light-gray"></span>
+                                                    <h6>Travel Place</h6>
+                                                </div>
+                                            </div>
+
+                                        </figure>
+                                    </li>
+                                    <!-- end portfolio item -->
+                                    <!-- start portfolio item -->
+                                    <li class="grid-item wow zoomIn last-paragraph-no-margin">
+                                        <figure>
+                                            <div class="portfolio-img bg-purple position-relative text-center overflow-hidden">
+                                                <img src="img/portfolio-4.jpg" alt=""/>
+                                                <div class="portfolio-icon text-white text-left">
+                                                    <p>Battle fields of </p>
+                                                    <p>Flanders, Belgium</p>
+                                                    <span class="seperator bg-light-gray"></span>
+                                                    <h6>Travel Place</h6>
+                                                </div>
+                                            </div>
+
+                                        </figure>
+                                    </li>
+                                    <!-- end portfolio item -->
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="profile-reviews">
+                    <h5>Reviews:</h5>
+
+                    <ul>
+                        <li>
+                            <p class="title">Impressive Achievement, Realstic and Shocking.</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 11November, 2016</p>
+                            <div class="rating">
+                                <ul class="profile-stars">
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                </ul>
+                                <span class="profile-points">5.0</span>
+                            </div>
+                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus. </p>
+                        </li>
+                        <li>
+                            <p class="title">Highly Recommended!</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 12January, 2017</p>
+                            <div class="rating">
+                                <ul class="profile-stars">
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                    <li><i  class="fa fa-star"></i></li>
+                                </ul>
+                                <span class="profile-points">5.0</span>
+                            </div>
+                            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus. </p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="profile-reviews profile-educatoin">
+                    <h5>Education:</h5>
+
+                    <ul>
+                        <li>
+                            <p class="title">HEC Lausanne - The Faculty of Business and Economics Faculty of the University of Lausanne</p>
+                            <p>Executive Master of Science in Technology Management</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 2004 - 2006</p>
+                        </li>
+                        <li>
+                            <p class="title">Notre-Dame de Jamhour, Jesuit College - Lebanon</p>
+                            <p>Executive Master of Science in Technology Management</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 1984 - 1997</p>
+                        </li>
+                        <li>
+                            <p class="title">University of Oxford !</p>
+                            <p>Executive Master of Science in Technology Management</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 2003 - 2004</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="profile-reviews profile-educatoin">
+                    <h5>Work Experience:</h5>
+                    <ul>
+                        <li>
+                            <p class="title">International Consultant</p>
+                            <p>CD Consulting</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 2010 - Present</p>
+                        </li>
+                        <li>
+                            <p class="title">International Consultant</p>
+                            <p>CD Consulting</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 2010 - Present</p>
+                        </li>
+                        <li>
+                            <p class="title">Business Anylist</p>
+                            <p>Consulting and Development Service</p>
+                            <p class="date"> <i class="fa fa-calendar"></i> 2006 - 2007</p>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once 'includes/footer.php' ?>
