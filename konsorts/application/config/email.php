fec6453a8601ddd613a4976b1c09632fb3756678
qudratullah1061 <qudratullah1061@gmail.com>
2017-12-23 17:36:01 +0500

    email cehck

---
+++ konsorts/application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['protocol'] = 'smtp';  
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'itcomradetest@gmail.com';
+$config['smtp_pass'] = 'itcomrade.us@123';
+$config['smtp_port'] = '465';
