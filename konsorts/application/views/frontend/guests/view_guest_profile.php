d3478e8cba5f8bc20becd46e367193185b113b7f
qudratullah1061 <qudratullah1061@gmail.com>
2017-12-24 04:42:48 +0500

    email cehck

--- konsorts/application/views/frontend/guests/view_guest_profile.php
+++ konsorts/application/views/frontend/guests/view_guest_profile.php
@@ -8,7 +8,7 @@
 <section class="pricing membership-plans">
     <div class="container">
 
-<h1 class="page-title"> Guest Member Profile | Account22
+<h1 class="page-title"> Guest Member Profile | Account
     <small>Guest account page</small>
 </h1>
 <!-- END PAGE TITLE-->
