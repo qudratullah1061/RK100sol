ffac35f183a2974c23c9cef536ad5040ec645a11
qudratullah1061 <qudratullah1061@gmail.com>
2017-12-23 16:28:38 +0500

    email cehck

--- konsorts/application/controllers/Misc.php
+++ konsorts/application/controllers/Misc.php
@@ -191,25 +191,25 @@ class Misc extends CI_Controller {
     function sendTestMail() {
         ini_set("SMTP", "ssl://smtp.gmail.com");
         ini_set("smtp_port", "465");
-        $to = "qudratullah1061@gmail.com";
-        $subject = "This is subject";
-
-        $message = "<b>This is HTML message.</b>";
-        $message .= "<h1>This is headline.</h1>";
-
-        $header = "From:qudrat.ullah@itcomrade.net \r\n";
-        $header .= "Cc:afgh@somedomain.com \r\n";
-        $header .= "MIME-Version: 1.0\r\n";
-        $header .= "Content-type: text/html\r\n";
-
-        $retval = mail($to, $subject, $message, $header);
-
-        if ($retval == true) {
-            echo "Message sent successfully...";
-        } else {
-            echo "Message could not be sent...";
-        }
-        exit;
+//        $to = "qudratullah1061@gmail.com";
+//        $subject = "This is subject";
+//
+//        $message = "<b>This is HTML message.</b>";
+//        $message .= "<h1>This is headline.</h1>";
+//
+//        $header = "From:qudrat.ullah@itcomrade.net \r\n";
+//        $header .= "Cc:afgh@somedomain.com \r\n";
+//        $header .= "MIME-Version: 1.0\r\n";
+//        $header .= "Content-type: text/html\r\n";
+//
+//        $retval = mail($to, $subject, $message, $header);
+//
+//        if ($retval == true) {
+//            echo "Message sent successfully...";
+//        } else {
+//            echo "Message could not be sent...";
+//        }
+//        exit;
         sendEmail("qudratullah1061@gmail.com", "Signup Successfull", "Registration completed. Please verify email by");
         exit;
     }
