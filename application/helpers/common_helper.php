9ccddea11aebebc93d0c700fcabb4f8bf5b2de55
qudratullah1061 <qudratullah1061@gmail.com>
2018-01-10 04:58:25 +0500

    email

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -315,16 +315,16 @@ function delete_file_from_directory($file_path) {
 }
 
 function sendEmail($to, $subject, $message) {
-    $header = "From:admin@konsorts.com \r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-    $retval = mail($to, $subject, $message, $header);
-    if ($retval == true) {
-        return true;
-    } else {
-        echo "Message could not be sent...";
-        exit;
-    }
+//    $header = "From:admin@konsorts.com \r\n";
+//    $header .= "MIME-Version: 1.0\r\n";
+//    $header .= "Content-type: text/html\r\n";
+//    $retval = mail($to, $subject, $message, $header);
+//    if ($retval == true) {
+//        return true;
+//    } else {
+//        echo "Message could not be sent...";
+//        exit;
+//    }
 //    
     global $CI;
     require APPPATH . 'libraries/phpmailer_master/PHPMailerAutoload.php';
