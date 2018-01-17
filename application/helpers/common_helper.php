fb6c946bfaae5f4fb31dd24e1e0ed4861253389f
qudratullah1061 <qudratullah1061@gmail.com>
2018-01-17 16:26:02 +0500

    about us + notifications system

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -411,8 +411,8 @@ function time_elapsed_string($datetime, $full = false) {
 }
 
 function sendEmail($to, $subject, $message, $reply_to = "") {
-    if ($reply_to) {
-        $headers = "From:admin@konsorts.com \r\n Reply-to: $reply_to";
+    if ($reply_to != "") {
+        $header = "From:admin@konsorts.com\r\nReply-to: $reply_to";
     } else {
         $header = "From:admin@konsorts.com \r\n";
     }
