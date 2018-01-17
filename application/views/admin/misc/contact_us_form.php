447111af9f95fe663d882c1fb302f2c3f07ad20c
qudratullah1061 <qudratullah1061@gmail.com>
2018-01-17 16:33:23 +0500

    about us + notifications system

--- application/views/admin/misc/contact_us_form.php
+++ application/views/admin/misc/contact_us_form.php
@@ -26,44 +26,32 @@
                         <a class="purple text-right" data-title="Add Type" href="javascript:Categories.modal_add_category()"><i class="fa fa-plus-circle"></i> Add new category</a>
                     </div>
                     <table id="contact_table" class="table table-striped table-bordered table-hover text-center dataTable no-footer" cellspacing="0" width="100%">
-                                                    <thead>
-                                                        <tr>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Subject</th>
+                                <th>Is Read</th>
+                                <th>Is Reply</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($contact_us_form as $value) { ?>
+                                <tr>
+                                    <td><?php echo $value->name; ?></td>
+                                    <td><?php echo $value->email; ?></td>
+                                    <td><?php echo $value->phone; ?></td>
+                                    <td><?php echo $value->subject; ?></td>
+                                    <td><?php echo ($value->is_read == 1 ? 'Yes' : 'No'); ?></td>
+                                    <td><?php echo ($value->is_reply == 1 ? 'Yes' : 'No'); ?></td>
+                                    <td><a class="btn btn-xs default btn-editable" onclick="Contact.modal_add_contact(<?php echo $value->contact_form_id; ?>)">View</a><a class="btn btn-xs default btn-editable" onclick="Contact.modal_add_contact_reply(<?php echo $value->contact_form_id; ?>)">Reply</a> </td>
+                                </tr>
+                            <?php } ?>
 
-                                                            <th>Name</th>
-                                                            <th>Email</th>
-                                                            <th>Phone</th>
-                                                            <th>Subject</th>
-                                                            
-                                                            
-
-
-                                                            
-                                                            <th>Is Read</th>
-                                                            <th>Is Reply</th>
-                                                            <th>Actions</th>
-                                                        </tr>
-                                                    </thead>
-
-                                                    <tbody>
-                                                        <?php foreach ($contact_us_form as $value) { ?>
-                                                            <tr>
-                                                                <td><?php echo $value->name;?></td>
-                                                                <td><?php echo $value->email; ?></td>
-                                                                <td><?php echo $value->phone; ?></td>
-                                                                <td><?php echo $value->subject; ?></td>
-                                                                
-                                                                <td><?php echo ($value->is_read == 1 ? 'Yes' : 'No'); ?></td>
-                                                                <td><?php echo ($value->is_reply == 1 ? 'Yes' : 'No'); ?></td>
-                                                               
-
-                                                                
-                                                                
-                                                                <td><a class="btn btn-xs default btn-editable" onclick="Contact.modal_add_contact(<?php echo $value->contact_form_id;?>)">View</a><a class="btn btn-xs default btn-editable" onclick="Contact.modal_add_contact_reply(<?php echo $value->contact_form_id;?>)">Reply</a> </td>
-                                                            </tr>
-                                                        <?php } ?>
-
-                                                    </tbody>
-                                                </table>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -72,8 +60,8 @@
 </div>
 
 <!-- End datatable-->
-<script src="<?php echo base_url(); ?>assets/frontend/datatable/jquery.dataTables.min.js" type="text/javascript"></script>
+<!--<script src="<?php // echo base_url(); ?>assets/frontend/datatable/jquery.dataTables.min.js" type="text/javascript"></script>-->
 <script src="<?php echo base_url('assets/custom_scripts/admin/contactus.js'); ?>" type="text/javascript"></script>
 <script>
-    $("#contact_table").DataTable({"scrollX": false});
+                                        $("#contact_table").DataTable({"scrollX": false});
 </script>
