ae71f27cb17604b2c5919feb0f549da4c4f4bc56
qudratullah1061 <qudratullah1061@gmail.com>
2018-03-13 18:58:30 +0500

    language comment

--- application/views/layouts/frontend/main.php
+++ application/views/layouts/frontend/main.php
@@ -160,7 +160,7 @@
                                         <?php
                                     }
                                     ?>
-                                    <li>
+<!--                                    <li>
                                         <a>
                                         <div id="google_translate_element"></div>
                                         </a>
@@ -170,7 +170,7 @@
                                         }
                                         </script>
                                         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-                                    </li>
+                                    </li>-->
                                 </ul>
                             </div>
                         </div>
