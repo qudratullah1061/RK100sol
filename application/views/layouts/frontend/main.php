00341472ca9737b55f64e988a3dde2b951fc58b6
qudratullah <qudratullah1061@gmail.com>
2018-09-10 02:55:38 +0500

    js issue fix.

--- application/views/layouts/frontend/main.php
+++ application/views/layouts/frontend/main.php
@@ -163,7 +163,7 @@
               type="text/css"/>
         <!--sweet alert ends-->
         <!-- BEGIN datepicker-->
-        <!--<link href="<?php // echo base_url();                                                                                                                               ?>assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />-->
+        <!--<link href="<?php // echo base_url();                                                                                                                                    ?>assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />-->
         <!--datepicker ends-->
         <!--select2 start-->
         <link href="<?php echo base_url(); ?>assets/global/plugins/select2/css/select2.min.css" rel="stylesheet"
@@ -275,7 +275,7 @@ if (isset($registration_completed) && $registration_completed) {
                                 <li class="<?php echo ActivateLink('about'); ?>">
                                     <a href="<?php echo base_url('about'); ?>">About</a>
                                 </li>
-                                <!--                                    <li class="<?php // echo ActivateLink('services');                                                                                                                 ?>">
+                                <!--                                    <li class="<?php // echo ActivateLink('services');                                                                                                                      ?>">
                                         <a href="javascript:void(0);">Our Services</a>
                                     </li>-->
                                 <li class="<?php echo ActivateLink('blog'); ?>">
@@ -362,23 +362,18 @@ if (isset($registration_completed) && $registration_completed) {
                                 if (count($connections) > 0) {
                                     ?>
                                     <div class="dropdown dropdown-extended dropdown-inbox" id="header_connection_bar">
-                                        <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"
-                                           data-hover="dropdown" data-close-others="true">
-                                            <a href="javascript:;" class="header-search-form text-white"><i
-                                                    class="fa fa-user-plus"></i>
-                                                    <?php
-                                                    $text = 'Connection';
-                                                    if (count($connections) > 1) {
-                                                        $text = 'Connections';
-                                                    }
-                                                    ?>
-                                            </a>
-
+                                        <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                            <i class="fa fa-user-plus"></i>
+                                            <?php
+                                            $text = 'Connection';
+                                            if (count($connections) > 1) {
+                                                $text = 'Connections';
+                                            }
+                                            ?>
                                         </a>
                                         <ul class="dropdown-menu">
                                             <li class="external">
-                                                <h3>You have <span
-                                                        class="bold"><?php echo count($connections) ?></span> <?php echo $text; ?>
+                                                <h3>You have <span class="bold"><?php echo count($connections) ?></span> <?php echo $text; ?>
                                                 </h3>
                                                 <a href="<?php echo site_url('member/connections') ?>">View All</a>
                                             </li>
@@ -547,11 +542,11 @@ if (isset($registration_completed) && $registration_completed) {
                                     <li class="enabled"><a
                                             href="<?php echo isset($admin_info['facebook_link']) ? $admin_info['facebook_link'] : ""; ?>"><i
                                                 class="fa fa-facebook-square"></i></a></li>
-                                <!--<li class="enabled"><a href="<?php // echo isset($admin_info[0]['youtube_link']) ? $admin_info[0]['youtube_link'] : "";                                                                                                                               ?>"><i class="fa fa-youtube-square"></i></a></li>-->
+                                <!--<li class="enabled"><a href="<?php // echo isset($admin_info[0]['youtube_link']) ? $admin_info[0]['youtube_link'] : "";                                                                                                                                    ?>"><i class="fa fa-youtube-square"></i></a></li>-->
                                     <li>
                                         <a href="<?php echo isset($admin_info['linkedin_link']) ? $admin_info['linkedin_link'] : ""; ?>"><i
                                                 class="fa fa-linkedin-square"></i></a></li>
-                                <!--<li><a href="<?php // echo isset($admin_info[0]['facebook_link']) ? $admin_info[0]['google_link'] : "";                                                                                                                               ?>"><i class="fa fa-google-plus-square"></i></a></li>-->
+                                <!--<li><a href="<?php // echo isset($admin_info[0]['facebook_link']) ? $admin_info[0]['google_link'] : "";                                                                                                                                    ?>"><i class="fa fa-google-plus-square"></i></a></li>-->
                                     <li class="enabled"><a
                                             href="<?php echo isset($admin_info['twitter_link']) ? $admin_info['twitter_link'] : ""; ?>"><i
                                                 class="fa fa-twitter-square"></i></a></li>
@@ -681,7 +676,7 @@ if (isset($registration_completed) && $registration_completed) {
     type="text/javascript"></script>
     <!--Form Validation end-->
     <!--datepicker start-->
-    <!--<script src="<?php // echo base_url();                                                                                                                               ?>assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>-->
+    <!--<script src="<?php // echo base_url();                                                                                                                                    ?>assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>-->
     <!--datepicker ends-->
     <!--select2 start-->
     <script src="<?php echo base_url(); ?>assets/global/plugins/select2/js/select2.full.min.js"
