dd5c50184298d5c25df132f0d791615271f6dcfb
qudratullah <qudratullah1061@gmail.com>
2018-09-11 04:41:23 +0500

    cron job update

--- application/controllers/Crons.php
+++ application/controllers/Crons.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
@@ -10,8 +11,10 @@ class Crons extends CI_Controller {
 
     public function updateSubscription() {
         $response = array();
+        $html = "";
         $users = $this->db->select('member_id,status,email,first_name')->where('end_subscription_date <= "' . date('Y-m-d H:i:s') . '" AND end_subscription_date != "0000-00-00 00:00:00" AND status!="suspended"')->get('tb_members')->result_array();
         if (isset($users) && count($users) > 0) {
+            $html += "Total Users To Suspend: " . count($users) . "<br/>";
             foreach ($users as $user) {
                 $member_email = $user['email'];
                 $macros_data['$$$FIRST_NAME$$$'] = $user['first_name'];
@@ -20,8 +23,9 @@ class Crons extends CI_Controller {
                     sendEmail($member_email, $email_template_info['template_subject'], $email_template_info['template_body']);
                     $this->db->set('status', 'suspended')->where('member_id', $user['member_id'])->update('tb_members');
                 }
+                $html += "$member_email <br/>";
             }
-            $response = array('error' => false, 'description' => "Cron Job Run Successfully!", 'code' => 200);
+            $response = array('error' => false, 'description' => "Cron Job Run Successfully!<br/>" . $html, 'code' => 200);
         } else {
             $response = array('error' => false, 'description' => "Cron Job Run Successfully! No record to suspend.", 'code' => 200);
         }
