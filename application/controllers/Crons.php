788c5cf86738373090f81c71161f8e4189210856
qudratullah <qudratullah1061@gmail.com>
2018-09-11 04:35:12 +0500

    Cron job.

--- application/controllers/Crons.php
+++ application/controllers/Crons.php
@@ -1,21 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Huma
- * Date: 9/7/2018
- * Time: 4:58 PM
- */
-
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Crons extends CI_Controller
-{
+class Crons extends CI_Controller {
+
     public function __construct() {
         parent::__construct();
     }
-    public function updateSubscription()
-    {
+
+    public function updateSubscription() {
+        $response = array();
         $users = $this->db->select('member_id,status,email,first_name')->where('end_subscription_date <= "' . date('Y-m-d H:i:s') . '" AND end_subscription_date != "0000-00-00 00:00:00" AND status!="suspended"')->get('tb_members')->result_array();
         if (isset($users) && count($users) > 0) {
             foreach ($users as $user) {
@@ -26,8 +20,13 @@ class Crons extends CI_Controller
                     sendEmail($member_email, $email_template_info['template_subject'], $email_template_info['template_body']);
                     $this->db->set('status', 'suspended')->where('member_id', $user['member_id'])->update('tb_members');
                 }
-
             }
+            $response = array('error' => false, 'description' => "Cron Job Run Successfully!", 'code' => 200);
+        } else {
+            $response = array('error' => false, 'description' => "Cron Job Run Successfully! No record to suspend.", 'code' => 200);
         }
+        echo json_encode($response);
+        exit;
     }
+
 }
