fa38c2ed7cefe38a6f44dd330576884cee60ebd4
qudratullah <qudratullah1061@gmail.com>
2018-09-11 04:44:51 +0500

    cron update

--- application/controllers/Crons.php
+++ application/controllers/Crons.php
@@ -14,16 +14,16 @@ class Crons extends CI_Controller {
         $html = "";
         $users = $this->db->select('member_id,status,email,first_name')->where('end_subscription_date <= "' . date('Y-m-d H:i:s') . '" AND end_subscription_date != "0000-00-00 00:00:00" AND status!="suspended"')->get('tb_members')->result_array();
         if (isset($users) && count($users) > 0) {
-            $html += "Total Users To Suspend: " . count($users) . "<br/>";
+            $html .= "Total Users To Suspend: " . count($users) . "<br/>";
             foreach ($users as $user) {
                 $member_email = $user['email'];
                 $macros_data['$$$FIRST_NAME$$$'] = $user['first_name'];
                 $email_template_info = get_email_template('member_suspended_subscription', $macros_data);
                 if ($email_template_info) {
-                    sendEmail($member_email, $email_template_info['template_subject'], $email_template_info['template_body']);
-                    $this->db->set('status', 'suspended')->where('member_id', $user['member_id'])->update('tb_members');
+//                    sendEmail($member_email, $email_template_info['template_subject'], $email_template_info['template_body']);
+//                    $this->db->set('status', 'suspended')->where('member_id', $user['member_id'])->update('tb_members');
                 }
-                $html += "$member_email <br/>";
+                $html .= "$member_email <br/>";
             }
             $response = array('error' => false, 'description' => "Cron Job Run Successfully!<br/>" . $html, 'code' => 200);
         } else {
